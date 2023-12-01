572dbcd9a5449642a05e70e88c1d0fae18c3d8ba
kevin tran <z5424745@ad.unsw.edu.au>
2023-12-02 01:15:05 +1100

    less ugly

--- d1/day1.py
+++ d1/day1.py
@@ -5,30 +5,19 @@ def sol(file):
     with open(file, 'r') as f:
         for lines in f:
             lines = re.sub(r'[a-zA-Z]', '', lines).rstrip()
-            llen = len(lines)
-            if llen == 1:
-                total += int(lines[0]*2)
-            else:                
-                total += int(lines[0] + lines[-1])
+            total += int(lines[0] + lines[-1]) 
     return total
 
 def sol2(file):
     l =  {'one': 1 , 'two': 2, 'three': 3, 'four': 4, 'five': 5, 'six': 6, 'seven': 7, 'eight': 8, 'nine': 9}
 
     total = 0
-    count = 1
     with open(file, 'r') as f:
         for lines in f:
-            print(str(count) + ' ', end='')
             for i in l:
                 lines = re.sub(rf"{i}", i + str(l[i]) + i, lines).rstrip()
             lines = re.sub(r'[a-zA-Z]', '', lines).rstrip()            
-            llen = len(lines)
-            if llen == 1:
-                total += int(lines[0]*2)
-            else:                
-                total += int(lines[0] + lines[-1])
-            count += 1
+            total += int(lines[0] + lines[-1]) 
     return total
 
 if __name__ == "__main__":
